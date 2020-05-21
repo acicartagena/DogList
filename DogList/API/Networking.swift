d5bb9b4abefc09eaf89f384cf4d5db66504e017a
acicartagena <acicartagena@gmail.com>
2020-05-21 23:47:49 +1000

    Added Networking implementation
    This implementation uses BrightFutures' futures and promises instead of doing blocks to handle asynchornous requests. Networking uses URLSession with default configuration. It includes making the network request and decoding the repsponse

--- DogList/API/Networking.swift
+++ DogList/API/Networking.swift
@@ -1,3 +1,41 @@
 //  Copyright © 2020 ACartagena. All rights reserved.
 
 import Foundation
+import BrightFutures
+
+class Networking {
+    let session = URLSession(configuration: .default)
+    private lazy var decoder: JSONDecoder = {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }()
+
+    func request<T:Decodable>(url: URL) -> Future<T,DogListError> {
+        let promise = Promise<T,DogListError>()
+
+        session.dataTask(with: url) { [weak self] (data, _, error) in
+            DispatchQueue.main.async {
+                if let error = error {
+                    promise.failure(.networking(error))
+                    return
+                }
+
+                guard let data = data, let strongSelf = self else {
+                    promise.failure(.noData)
+                    return
+                }
+
+                do {
+                    let decoded = try strongSelf.decoder.decode(T.self, from: data)
+                    promise.success(decoded)
+                } catch let error {
+                    promise.failure(.decoding(error))
+                    return
+                }
+            }
+        }
+        .resume()
+        return promise.future
+    }
+}
